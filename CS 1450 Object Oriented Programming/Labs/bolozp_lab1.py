0acc097c0a1ca1788718ab6cfe1741928587bdaf
Patrik B <35575791+patrikboloz@users.noreply.github.com>
2024-02-22 15:55:54 -0700

    Add files via upload

---
+++ CS 1450 Object Oriented Programming/Labs/bolozp_lab1.py
@@ -0,0 +1,3 @@
+#CS 1450 Lab 1
+#Your task will be to create a class called StandingFan(), which by using methods will be able to turn on/off rotation and increase/decrease the speed of the fan (0 - fan turned off, 5 - maximum speed). 
+#HINT:Use attributes fan_speed = 0 and rotation = False as a starting point.
