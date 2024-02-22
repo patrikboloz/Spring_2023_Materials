ad3ed0481285dbff61d8604f531acbcb2233c5ea
Patrik B <35575791+patrikboloz@users.noreply.github.com>
2024-02-22 15:56:18 -0700

    Add files via upload

---
+++ CS 1450 Object Oriented Programming/Midterm/midterm.py
@@ -0,0 +1,28 @@
+""" Midterm
+
+Create these three classes and either have them all in one file or
+you can have seperate files for all classes. (it's your choice)
+Each successfully created and tested class will be worth 50 points.
+
+1) Create a class called Mood. This class will start with an attribute
+called self.computer_mood = "Neutral". Then create these 4 methods called 
+angry, sad, happy, and neutral, which will change self.computer_mood to 
+their respective moods and will print out a message, how the computer feels, 
+when those methods are called/used. Test the class by creating an object from 
+the class and use all methods to change the computer's mood.
+
+2) Create a class called Calculator, that will have two arguments in
+__init__() called: num1, num2. Then create two attributes self.num1 and
+self.num2 that will be used throughout the Calculator class. Create 4
+methods called class_sum, class_div, class_add, class_mult to do summation,
+division, addition, and multiplication, respectively. Then test it out
+on the two numbers, that will be chosen by using the user's input.
+
+3) Create a class called School_Class, that starts with an attribute called 
+self.class_list, which will start as an empty list. Create three methods 
+called add_student, delete_student, change_student. Create an interactive 
+menu, where a user will be able use those three methods to add a student
+to the list, delete a student from the list, and change the student's name.
+(take a look at our earlier examples of an interactive menu with Bank and 
+Account classes). Then test it out on your interactive menu. 
+""" 
